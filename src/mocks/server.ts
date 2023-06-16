d44e9390da0d19b77a88b6a5efe57b61943e1ebf
Salman Jabbari <olddealers@gmail.com>
2023-06-16 16:47:47 +0330

    configure mock api and db

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,4 +1,5 @@
 import { createServer, Model, RestSerializer } from 'miragejs';
+import db from './db.json';
 
 export function makeServer({ environment = 'test' } = {}) {
   const server = createServer({
@@ -15,25 +16,41 @@ export function makeServer({ environment = 'test' } = {}) {
     routes() {
       this.namespace = 'api';
 
-      this.get('/map', (schema) => {
-        return ['m213', 'm654', 'm63', 'm6888'];
+      this.get('/map', () => {
+        return Object.keys(db.stadium.map);
       });
 
       this.get('/map/:map_id', (schema, request) => {
         const map_id = request.params.map_id;
-        // you can return different responses based on the map_id
-        return [
-          [0, 0, 1, 0],
-          [0, 1, 0, 0],
-          [1, 1, 1, 1],
-          [1, 1, 1, 1],
-        ];
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        return db.stadium.map[map_id].seats || [];
       });
 
       this.post('/map/:map_id/ticket', (schema, request) => {
         const requestBody = JSON.parse(request.requestBody);
-        // handle your request here
-        return { status: 'success' };
+        const map_id = request.params.map_id;
+        const x = requestBody.x;
+        const y = requestBody.y;
+
+        // Check if the seat is available
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        if (db.stadium.map[map_id].seats[x][y] === 0) {
+          // Reserve the seat
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          db.stadium.map[map_id].seats[x][y] = 1;
+
+          // Update tickets in DB
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          db.tickets.push({ x, y, map_id });
+
+          return { status: 'success' };
+        } else {
+          return { status: 'failed', message: 'Seat is already reserved.' };
+        }
       });
     },
   });
